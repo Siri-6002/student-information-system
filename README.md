bb34086722b68d42c79e54b264e38911df7de1ec
Siri Ramagiri <siriramagiri6@gmail.com>
2026-06-20 08:30:59 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Student Information System
+
+This project stores and displays student information.
+
+Created for SITER Academy Git Assignment.
---
+++ student.py
@@ -0,0 +1,8 @@
+name = input("Enter Student Name: ")
+roll = input("Enter Roll Number: ")
+branch = input("Enter Branch: ")
+
+print("\nStudent Details")
+print("Name:", name)
+print("Roll Number:", roll)
+print("Branch:", branch)
